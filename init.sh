13d79ae24886abed5e4023dd723261143022e94e
Viktor Mishyn <viktormishyn@gmail.com>
2021-01-01 12:10:42 +0200

    Delete empty folders

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # this script automates creating a number of folders for the course assignments repository
 # each folder is called <Task{n}> and contains empty readme.md file
 
-for n in {1..15}; do
+for n in {1..12}; do
   mkdir "Task$n"
 done;
 
